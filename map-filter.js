fbc059aa4cd772f1c4f50fb6452e6abd668a2717
Injamul Haque Rashik <ihrashik@gmail.com>
2020-03-10 01:09:49 +0600

    map and filter added!

---
+++ map-filter.js
@@ -0,0 +1,23 @@
+const numbers = [3, 4, 5, 6, 7, 8];
+// // const output = [];
+// // for (let i = 0; i < numbers.length; i++) {
+// //   const element = numbers[i];
+// //   const result = element * element;
+// //   output.push(result);
+// // }
+
+// function square(element) {
+//   return element;
+// }
+
+// const result = numbers.map(function test(element) {
+//   console.log(element);
+// });
+
+// //console.log(result);
+
+const bigger = numbers.filter(x => x > 5); // filter shows/returns an array
+
+const biggerFind = numbers.find(x => x > 5); // find shows/returns an element of an array
+
+console.log(biggerFind);
