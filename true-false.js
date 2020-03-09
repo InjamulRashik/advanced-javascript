4c1cc107e4a855ddf9340d87ff4688c25c0a8de1
Injamul Haque Rashik <ihrashik@gmail.com>
2020-03-10 00:27:32 +0600

    true-false Added!

---
+++ true-false.js
@@ -0,0 +1,16 @@
+// Falsies
+// 0
+// ""
+// undefined
+// null
+// NaN
+
+// Truthy
+// "0"," ",[]
+let name = null;
+console.log(name);
+if (name) {
+  console.log("condition is true");
+} else {
+  console.log("condition is false");
+}
