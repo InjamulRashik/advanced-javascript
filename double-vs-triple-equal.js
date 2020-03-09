7aa66d5de1c60aeae4c467d3d77311068749ea41
Injamul Haque Rashik <ihrashik@gmail.com>
2020-03-10 00:51:12 +0600

    Triple Equal Added!

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,12 @@
+// Implicit Condition Checker
+// "==" for Same Data Type && To check Value
+// "===" for Implicit Data Type && To Check Data Type and Value
+
+const first = 2;
+const second = 3;
+
+if (first == second) {
+  console.log("True");
+} else {
+  console.log("False");
+}
