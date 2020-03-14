0f4ccd160701faf10c7f2b68603aa1b5badc6a03
Injamul Haque Rashik <ihrashik@gmail.com>
2020-03-15 02:54:45 +0600

    break-continue added!

---
+++ break-continue.js
@@ -0,0 +1,12 @@
+const numbs = [1, -2, 3, 4, -5, -6, 7, -8, 9];
+for (let i = 0; i < numbs.length; i++) {
+  //   console.log(numbs[i]);
+  if (numbs[i] > 3) {
+    break;
+  }
+}
+for (let i = 0; i < numbs.length; i++) {
+  if (numbs[i] < 0) {
+  }
+  console.log(numbs[i]);
+}
